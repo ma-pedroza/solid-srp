e7229fb1650cafb443233d7d368d3afc7a828e19
Rayssa Marconato <rayssa.g.marconato@gmail.com>
2025-10-10 21:42:45 -0300

    feat: add initial files

---
+++ projeto2/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rayss/projeto2',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ca5c12cf522f98e4bffc3d3d817b82f528be266e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rayss/projeto2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ca5c12cf522f98e4bffc3d3d817b82f528be266e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '4.0.0',
+            'version' => '4.0.0.0',
+            'reference' => '06113cfdaf117fc2165f9cd040bd0f17fcd5242d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
