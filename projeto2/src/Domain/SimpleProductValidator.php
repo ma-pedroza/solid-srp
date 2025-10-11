a0b26d2c002628ac2d60beb7d429dfe8ebf45e28
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-10-11 20:42:52 -0300

    starting product validation
    Add product validator

---
+++ projeto2/src/Domain/SimpleProductValidator.php
@@ -0,0 +1,27 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Domain;
+
+final class SimpleProductValidator
+{
+    /**
+     * @param arayy{name?:string,price?:float} $input
+     */
+
+    public function validate(array $input): array
+    {
+        $errors = [];
+
+        if (strlen($input['name']) <2 or strlen($input['name']) > 100) {
+            $errors[] = 'Nome deve conter entre 2 e 100 caracteres';
+        }
+
+        if ($input['price'] >= 0) {
+            $errors[] = 'Preço deve ser maior do que 0';
+        }
+
+        return $errors;
+    }
+}
